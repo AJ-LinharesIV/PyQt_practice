a77fa8a1823a8c065ce6f03e45fe1c2ee8c766ba
AJ <anthony.linharesiv@gmail.com>
2024-01-31 23:30:38 -0600

    working prophet model example and plotting

--- predictor_gui_ajl.py
+++ predictor_gui_ajl.py
@@ -2,29 +2,19 @@
 # Python Package Imports
 import sys
 import pandas as pd
-import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from PyQt5 import QtWidgets
 from prophet import Prophet
+from datetime import date
 
 # App Imports
 from predictor_gui_pyqt_output import Ui_MainWindow
 
-class MatplotlibWidget(FigureCanvasQTAgg):
+class MplWidget(FigureCanvasQTAgg):
 
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        self.fig, self.axes = plt.subplots(figsize=(width, height), dpi=dpi)
-        plt.rcParams.update({'font.weight': 'normal', 'font.size': 16}) # set font size and weight on the plot
-        FigureCanvasQTAgg.__init__(self, self.fig)
+    def __init__(self, figure, parent=None):
+        FigureCanvasQTAgg.__init__(self, figure)
         self.setParent(parent)
-
-    def plot(self, x, y):
-        self.axes.clear()
-        self.axes.plot(x, y)
-        self.axes.set_title('Plot of y vs. x', fontweight='bold')
-        self.axes.set_xlabel('x', fontweight='bold')
-        self.axes.set_ylabel('y', fontweight='bold')
-        self.draw()
 
     def save_bool(self, savefile='')->bool:
         if savefile:
@@ -42,18 +32,7 @@ class Predictor_UI_MainWindow(Ui_MainWindow):
         # so setupUi has to be called manually
         self.setupUi(MainWindow=main_wind)
 
-        # define instance of the matplotlib Canvas widget
-        self.mpl_widge = MatplotlibWidget(width=5, height=4, dpi=45)
-        self.mpl_widge.plot([0, 1], [0, 0])
-
-        # add the matplotlib widget to the main window
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(self.mpl_widge)
-        widget = QtWidgets.QWidget()
-        widget.setLayout(layout)
-        self.verticalLayout_4.addWidget(widget)
-
-        # connect signals & slots on initialization
+        # connect signals & slots
         self.sig_slot_connects()
 
     def sig_slot_connects(self):
@@ -62,32 +41,16 @@ class Predictor_UI_MainWindow(Ui_MainWindow):
 
     def predict_button_click(self):
 
-        # read csv with data
-        data = self.get_data_from_file()
-        #future_data = self.train_prophet_model(data)
-        #print(f'data returned from prophet model:{future_data}')
-        #print(f'type of prophet model data:{type(future_data)}')
-        #future_data.to_csv('prophetmodelresults.csv')
-
-
         save_choice = self.checkSavePlotToFile.isChecked()
         s_file = self.linePlotFile.text()
 
-        self.mpl_widge.plot(data['x'], data['y'])
-        if save_choice:
-            save_success = self.mpl_widge.save_bool(savefile=s_file)
-            if not save_success:
-                self.textBrowserOutput.append(f'No Plot Output File Name Provided.\nTry again.')
-
-    def get_data_from_file(self)->pd.DataFrame:
-        f_name = self.lineInputDataFile.text()
+        input_fname = self.lineInputDataFile.text()
 
         try:
-            data = pd.read_csv(f_name)
-            return data
+            data = pd.read_csv(input_fname)
+            self.train_prophet_model(data, save_plot=save_choice, s_file=s_file)
         except FileNotFoundError:
-            self.textBrowserOutput.append(f'file {f_name} not found in the current directory.\nTry again.')
-            return pd.DataFrame({'x': [0, 0], 'y': [0, 0]})
+            self.textBrowserOutput.append(f'file {input_fname} not found in the current directory.\nTry again.')
 
     def input_file_click(self):
 
@@ -98,13 +61,64 @@ class Predictor_UI_MainWindow(Ui_MainWindow):
         if fname:
             self.lineInputDataFile.setText(fname[0])
 
-    @staticmethod
-    def train_prophet_model(data)->pd.DataFrame:
+    def train_prophet_model(self, data: pd.DataFrame, save_plot: bool, s_file: str):
+
+        self.textBrowserOutput.append('Fitting Prophet regression model to data...\n')
+
+        # instantiate a Prophet model
         m = Prophet()
+
+        # regression fit the input data
         m.fit(data)
-        future = m.make_future_dataframe(periods=365)
+
+        # calculate the number of extra days to add to the future dataframe
+        ld_ls = [int(i) for i in str(data['ds'].iloc[-1]).split(sep='-')]
+        last_date = date(ld_ls[0], ld_ls[1], ld_ls[2])
+        to_date = self.datePredictEnd.date().toPyDate()
+        delta = to_date - last_date
+
+        if delta.days > 0:
+            future = m.make_future_dataframe(periods=delta.days)
+        else:
+            self.textBrowserOutput.append(f'date {self.datePredictEnd.date().toPyDate()} is before or '
+                                   f'concurrent with last date in input dataset...\n'
+                                   f'Arbitrarily setting end date 365 days after last date in'
+                                   f'dataset.\nTry again if this isnt what you want.\n')
+            future = m.make_future_dataframe(periods=365)
+
+        # use Prophet predict method to forecast into the future
+        self.textBrowserOutput.append('Forecasting future data...\n')
         forecast = m.predict(future)
-        return forecast
+
+        # get the predicted value for the specific date requested by user
+        spec_date = str(self.datePredictSpec.date().toPyDate())
+        if spec_date in forecast['ds'].values:
+            fc = forecast.set_index('ds')
+            spec_yhat = fc['yhat'].loc[spec_date]
+            self.linePredValue.text(str(round(spec_yhat,3)))
+        else:
+            self.textBrowserOutput.append('Requested date doesnt appear in dataset...\nTry Again.\n')
+
+        # delete placeholder graphicsView widge from verticalLayout4 in prep for Prophet plot
+        self.graphicsView.close()
+        self.verticalLayout_4.update()
+
+        # create an instance of the MplWidget using the Prophet .plot() method as the figure
+        mpl_widget = MplWidget(figure=m.plot(forecast, figsize=(4,5), include_legend=True))
+
+        # add the matplotlib widget to the main window
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(mpl_widget)
+        widget = QtWidgets.QWidget()
+        widget.setLayout(layout)
+
+        self.verticalLayout_4.addWidget(widget)
+
+        # save plot if reqd
+        if save_plot:
+            save_success = mpl_widget.save_bool(savefile=s_file)
+            if not save_success:
+                self.textBrowserOutput.append(f'No Plot Output File Name Provided.\nTry again.')
 
 # drive application
 if __name__ == "__main__":
